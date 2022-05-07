f120cd652a981a53dd53202a115ed3e8cfa14e6c
Jonata Gomes <jonatajonatagomes@gmail.com>
2022-05-07 02:59:00 -0300

    ajustes de largura de tela. criador a responsividade para descktop. interação com JS

--- main.js
+++ main.js
@@ -4,6 +4,49 @@ onScroll()
 function onScroll() {
    showNavOnScroll()
    showBackToTopButtonOnScroll()
+
+   activateMenuCurrentSection(home)
+   activateMenuCurrentSection(services)
+   activateMenuCurrentSection(about)
+   activateMenuCurrentSection(contact)
+}
+
+function activateMenuCurrentSection(section) {
+    //linha alvo
+    const targetLine = scrollY + innerHeight / 2
+    
+    //verificar se a seção passou da linha
+    //quais dados vou precisar
+    //o topo da seção
+    const sectionTop = section.offsetTop
+    //altura da seção
+    const sectionHeight = section.offsetHeight
+    //o topo da seção chegou ou ultrapassou a linha alto
+    const sectionTopRachOrPassedTargetline = targetLine >= sectionTop
+    
+    //informações dos dados e da logica
+  //  console.log('o topo da seção chegou ou passou da linha?', sectionTopRachOrPassedTargetline)
+
+    //verificar se a base está abixo da linha alvo
+    //quais dados vou precisar?
+    //a seção termina onde?
+     const sectionEndsAt = sectionTop + sectionHeight
+    //o final da seção passou da linha alvo
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+ //  console.log('o fundo da seção passou da linha: ', sectionEndPassedTargetLine)
+
+    //limites da seção
+    const sectionBondaries = sectionTopRachOrPassedTargetline && !sectionEndPassedTargetLine
+
+ // console.log(sectionBondaries)
+
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+    menuElement.classList.remove('active')
+    if (sectionBondaries) {
+        menuElement.classList.add('active')
+    }
 }
 
 function showNavOnScroll() {
@@ -21,7 +64,6 @@ function showBackToTopButtonOnScroll() {
         backToTopButton.classList.remove('show')
     }
 }
-
 
 function openMenu() {
     document.body.classList.add('menu-expanded')
@@ -44,4 +86,6 @@ ScrollReveal({
 #about,
 #about header,
 #about .content,
-#contact`);
+#contact,
+#contact header,
+#contact .content`);
